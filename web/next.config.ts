c3014afc7dad7aca3eef765ff3ef5df1f26ad686
纯真浩一郎 <72e1bcgg@gmail.com>
2025-12-05 13:42:30 +0800

    add image function

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,5 +1,26 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+    dangerouslyAllowLocalIP: true,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
